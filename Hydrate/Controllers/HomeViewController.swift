27d70a566c69f348ad9e760022d27744cfbb6f36
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-03-17 16:17:38 -0700

    finished login button

---
+++ Hydrate/Controllers/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Hydrate
+//
+//  Created by Gonzalo Birrueta on 3/17/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import TransitionButton
+
+class HomeViewController: CustomTransitionViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+    
+    
+}
--- Hydrate/Controllers/LoginViewController.swift
+++ Hydrate/Controllers/LoginViewController.swift
@@ -42,7 +42,8 @@ class LoginViewController: UIViewController{
         stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         stackView .centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         
-        
+
+
         
         let passwordTextField = CustomAnimatedTextField(placeholderText: "Password", floatedText: "Enter Password", selectedColor: UIColor(red:0.00, green:0.46, blue:0.60, alpha:1.0), icon: UIImage(systemName: "lock.fill")!, secure: true)
         
@@ -53,6 +54,54 @@ class LoginViewController: UIViewController{
         
         usernameTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        let loginButton = TransitionButton(frame: CGRect(x: 100, y: 100, width: 100, height: 40))
+        loginButton.backgroundColor = UIColor(red:0.00, green:0.46, blue:0.60, alpha:1.0)
+        loginButton.setTitle("SIGN IN", for: .normal)
+        loginButton.cornerRadius = 20
+        loginButton.spinnerColor = .white
+        loginButton.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
+        
+        self.view.addSubview(loginButton)
+        
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 30.0).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+        loginButton.widthAnchor.constraint(equalToConstant: 150.0).isActive = true
+        loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        loginButton.tintColor = .white
+        loginButton.titleLabel!.font = UIFont(name: "Helvetica-Bold", size: 18)
+        loginButton.semanticContentAttribute = .forceRightToLeft
+        loginButton.setImage(UIImage(systemName: "arrow.left"), for: .normal)
+    }
+    
+    
+    
+    
+    
+    
+    
+    @objc func loginTapped(_ sender: TransitionButton){
+        sender.startAnimation() // 2: Then start the animation when the user tap the button
+        let qualityOfServiceClass = DispatchQoS.QoSClass.background
+        let backgroundQueue = DispatchQueue.global(qos: qualityOfServiceClass)
+        backgroundQueue.async(execute: {
+            
+            sleep(2) // 3: Do your networking task or background work here.
+            
+            DispatchQueue.main.async(execute: { () -> Void in
+                // 4: Stop the animation, here you have three options for the `animationStyle` property:
+                // .expand: useful when the task has been compeletd successfully and you want to expand the button and transit to another view controller in the completion callback
+                // .shake: when you want to reflect to the user that the task did not complete successfly
+                // .normal
+                sender.stopAnimation(animationStyle: .normal, completion: {
+                    let homeVC = HomeViewController()
+                    self.navigationController?.pushViewController(homeVC, animated: true)
+//                    self.present(homeVC, animated: true, completion: nil)
+                })
+            })
+        })
     }
 
 }
